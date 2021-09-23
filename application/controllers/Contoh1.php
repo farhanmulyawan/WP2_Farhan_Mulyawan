6c16252243940bc4d6231baa13784a6db4825299
Farhan Mulyawan <farhan.080201@gmail.com>
2021-09-23 12:00:44 +0700

    pertemuan 1

--- application/controllers/Contoh1.php
+++ application/controllers/Contoh1.php
@@ -4,6 +4,6 @@ class Contoh1 extends CI_Controller
     public function index()
     {
         echo "<h1>Perkenalkan</h1>";
-        echo "<h1>Perkenalkan</h1>"; echo"Nama saya Farhan Mulyawan Saya tingga di daerah Rawamangun olah raga yang saya sukai adalah Sepak Bola";
+        echo"Nama saya Farhan Mulyawan Saya tingga di daerah Rawamangun olah raga yang saya sukai adalah Sepak Bola";
     }
 }
